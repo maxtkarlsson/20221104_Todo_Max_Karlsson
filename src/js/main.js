0214d1e1363798ba1467f1a5ac9dc3f99222cacf
maxtkarlsson <max.karlsson93@gmail.com>
2022-11-02 15:31:27 +0100

    Added 'add task' function

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,56 @@
 class Task {
-  constructor(header, desc) {
-    this.header = header;
-    this.desc = desc;
-  }
-}
-document.getElementById("btnSubmit").addEventListener("click", submit);
-
-let taskList = [];
-
-function submit() {
-  let inputTask = document.getElementById("inputTask");
-  let inputDesc = document.getElementById("inputDesc");
-
-  taskList.push(new Task(inputTask.value, inputDesc.value));
-
-  console.log(taskList);
-
-  for (let i = 0; i < taskList.length; i++) {
-    document.getElementById("todoList");
+  constructor(name, completed) {
+    this.name = name;
+    this.completed = completed; //True or false
   }
 }
 
-// Checkbox har boolean värde, använda det kanske?
+let pendingTasks = [new Task("Köpa mjölk", false), new Task("Träna", false)];
+
+function writePendingTasks() {
+  let listPending = document.getElementById("listPending");
+  listPending.innerHTML = "";
+
+  for (let i = 0; i < pendingTasks.length; i++) {
+    let item = document.createElement("li");
+    item.innerText = pendingTasks[i].name;
+
+    if (pendingTasks[i].completed !== true) {
+      item.className = "list__item";
+      listPending.appendChild(item);
+    } else {
+      item.className = "list__item--completed";
+      listCompleted.appendChild(item);
+    }
+  }
+}
+
+writePendingTasks();
+
+document.getElementById("btnSubmit").addEventListener("click", addTask);
+
+function addTask() {
+  let inputName = document.getElementById("inputTask");
+
+  pendingTasks.push(new Task(inputName.value, false));
+  writePendingTasks();
+}
+
+// hur skriva ut en lista med object i HTML?
+
+//Tankar:
+// Checkbox har boolean värde, använda det för att göra treu or false
 // Sen ha olika objekt som flyttas mellan sections via parents?
-// Sorera på datum kanske?
+// Sortera på datum kanske?
+
+/*
+Upplägg
+
+1. En användare matar in en task som skapas och hamnar i listan pending.
+
+2. En användare klickar på en task i listan pending som hamnar i listan completed
+
+3. En användare klickar på en task i listan completed som hamnar i listan pending
+
+
+*/
