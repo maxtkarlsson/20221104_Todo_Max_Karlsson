335cc05313bbc91ca3e0113a8370cb71853f24c1
maxtkarlsson <max.karlsson93@gmail.com>
2022-11-04 14:03:49 +0100

    Cleaned up code, added comments and split js file up into two

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,8 @@
-class Task {
-  constructor(name, completed) {
-    this.name = name;
-    this.completed = completed; //True or false
-  }
-}
+import { Task } from "./models/task";
 
-let tasks = [new Task("Köpa mjölk", false), new Task("Träna", true)];
+let tasks = [new Task("Köpa mjölk", false), new Task("Träna", false)];
 
+// En funktion som kollar värdet på en viss tasks "completed" egenskap, true/false, för att sedan ändra till motsatsen:
 function toggleComplete(task) {
   if (task.completed == false) {
     task.completed = true;
@@ -14,39 +10,33 @@ function toggleComplete(task) {
     task.completed = false;
   }
 
+  // Ser till att skriva ut listan igen för att uppdatera listorna i HTML.
   writeTasks();
 }
-/*
-Ovan funktion fast på "modernare" sätt:
 
-const toggleComplete = (task) => {
-  if (task.completed == false) {
-    task.completed = true;
-  } else {
-    task.completed = false;
-  }
-  writeTasks();
-};
-*/
-
+// En funktion som skriver ut vår lista i html
 function writeTasks() {
+  //Ser till att vi kan arbeta med de två ul elementen i funktionen:
   let listPending = document.getElementById("listPending");
   let listCompleted = document.getElementById("listCompleted");
+
+  //Ser till att rensa tidigare innehåll i dessa:
   listPending.innerHTML = "";
   listCompleted.innerHTML = "";
 
+  // Loopar igenom vår lista med tasks.
   for (let i = 0; i < tasks.length; i++) {
+    //Lägger till objektens namn i <li> element och ser till att det lyssnar efter klick för att kunna markera som klar/ ej klar.
     let task = document.createElement("li");
     task.innerText = tasks[i].name;
-    //task.id = tasks[i].name;
     task.addEventListener("click", () => {
       toggleComplete(tasks[i]);
     });
+
     /*
-    item.addEventListener("click", () => {
-      toggleComplete(tasks[i]);
-    });
-*/
+    If satsen kontrollerar i vilket av de två list-elementen ett <li> element ska läggas i beroende på om tasken är pending eller completed.
+    Lägger även till klassnamn för stylingens skull.
+    */
     if (tasks[i].completed !== true) {
       task.className = "list__item";
       listPending.appendChild(task);
@@ -55,33 +45,23 @@ function writeTasks() {
       listCompleted.appendChild(task);
     }
   }
-  console.log(tasks);
 }
-
-function addTask() {
-  tasks.push(new Task(document.getElementById("inputTask").value, false));
-  writeTasks();
-}
-
-writeTasks();
-
-document.getElementById("btnSubmit").addEventListener("click", addTask);
-
-// hur skriva ut en lista med object i HTML?
-
-//Tankar:
-// Checkbox har boolean värde, använda det för att göra treu or false
-// Sen ha olika objekt som flyttas mellan sections via parents?
-// Sortera på datum kanske?
 
 /*
-Upplägg
-
-1. En användare matar in en task som skapas och hamnar i listan pending.
-
-2. En användare klickar på en task i listan pending som hamnar i listan completed
-
-3. En användare klickar på en task i listan completed som hamnar i listan pending
-
-
+En funktion som lägger till en task utifrån vår klass och användarens värde i textrutan. Completed är false då uppgiften är pending.
+Listan skrivs ut igen och textrutan rensas.
+If satsen säkerställer att man inte kan lägga till en tom task.
 */
+function addTask() {
+  if (document.getElementById("inputTask").value != "") {
+    tasks.push(new Task(document.getElementById("inputTask").value, false));
+    writeTasks();
+    document.getElementById("inputTask").value = "";
+  }
+}
+
+// Ser till att knappen lyssnar efter klick och vid klick akriverar vår addTask funktion
+document.getElementById("btnSubmit").addEventListener("click", addTask);
+
+//Skriver ut den befintliga listan när JS filen laddas för första gången.
+writeTasks();
---
+++ src/js/models/task.js
@@ -0,0 +1,6 @@
+export class Task {
+  constructor(name, completed) {
+    this.name = name;
+    this.completed = completed;
+  }
+}
